ea822701eab4178222a22557308f43ee6b0ce22e
ClayTMS <bboyClay89@gmail.com>
2022-11-13 21:51:32 +0300

    Second

--- Main.java
+++ Main.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//            String s = "null";
+//            Optional<String> opt = Optional.ofNullable(s);
+//            opt.ifPresentOrElse(
+//                    x -> System.out.println(x),
+//            () -> System.out.println("No value"));
+
+        List<Integer> list = new ArrayList<>();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите колличесво эллементов в коллекции: ");
+            if (scanner.hasNextInt()) {
+                int elemCollect = scanner.nextInt();
+
+                for (int i = 0; i < elemCollect; i++) {
+                    list.add((int) (Math.random() * 5));
+                }
+            }
+            System.out.println("Коллекция сейчас: " + list);
+            list.stream().distinct().forEach(System.out::print);
+
+        }
+
 
     }
 }
